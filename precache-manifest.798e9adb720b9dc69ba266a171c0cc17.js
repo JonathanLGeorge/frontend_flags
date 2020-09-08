ee10b780755808f8906bcae808d0b7b1e4bbc14c
Jonathan <jonathan.george.dev@gmail.com>
2020-09-08 04:01:27 -0700

    Updates

---
+++ precache-manifest.798e9adb720b9dc69ba266a171c0cc17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85fd314a82e03e834275e42e5feb23f",
+    "url": "/frontend_flags/index.html"
+  },
+  {
+    "revision": "d640178ed0ba3294cf8d",
+    "url": "/frontend_flags/static/css/main.89d8f3e9.chunk.css"
+  },
+  {
+    "revision": "6db47d7a2d7d7e1fce78",
+    "url": "/frontend_flags/static/js/2.23b3d877.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend_flags/static/js/2.23b3d877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d640178ed0ba3294cf8d",
+    "url": "/frontend_flags/static/js/main.7b5fde43.chunk.js"
+  },
+  {
+    "revision": "b5f09292aa42cc6918ba",
+    "url": "/frontend_flags/static/js/runtime-main.18132e96.js"
+  },
+  {
+    "revision": "5ed028d98b774623f35a0fb9cb784818",
+    "url": "/frontend_flags/static/media/sprites.5ed028d9.svg"
+  }
+]);
